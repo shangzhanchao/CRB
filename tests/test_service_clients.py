80128e0d698d75558453add363307bdb5b72d9fc
shangzhanchao <15863174575@163.com>
2025-07-30 21:19:05 +0800

    Update test_service_clients.py

--- tests/test_service_clients.py
+++ tests/test_service_clients.py
@@ -1,9 +1,12 @@
 import unittest
-from ai_core.service_clients import (
+import os
+from ai_core.service_api import (
     call_asr,
     call_tts,
     call_llm,
     call_voiceprint,
+    call_memory_save,
+    call_memory_query,
 )
 
 
@@ -14,6 +17,15 @@ class TestServiceClients(unittest.TestCase):
         self.assertEqual(call_voiceprint("user1.wav", url=bad_url), "user1")
         self.assertEqual(call_llm("hi", url=bad_url), "")
         self.assertEqual(call_tts("hello", url=bad_url), "")
+        tmp = "tmp_memory.json"
+        if os.path.exists(tmp):
+            os.remove(tmp)
+        self.assertFalse(os.path.exists(tmp))
+        self.assertFalse(call_memory_save({"text": "hi"}, url=bad_url, fallback_path=tmp))
+        self.assertTrue(os.path.exists(tmp))
+        res = call_memory_query("hi", url=bad_url, fallback_path=tmp)
+        self.assertIsInstance(res, list)
+        os.remove(tmp)
 
 
 if __name__ == "__main__":
