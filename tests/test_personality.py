16c1fa679f8d6906843b65edd9247eabd531209f
shangzhanchao <15863174575@163.com>
2025-07-30 08:48:49 +0800

    Update test_personality.py

--- tests/test_personality.py
+++ tests/test_personality.py
@@ -4,7 +4,8 @@ from ai_core import PersonalityEngine
 class TestPersonalityEngine(unittest.TestCase):
     def test_update_and_style(self):
         engine = PersonalityEngine(momentum=0.5)
-        self.assertEqual(engine.vector, [0.0] * 5)
+        # default vector should emphasize extraversion
+        self.assertEqual(engine.vector[2], 1.0)
         engine.update('praise')
         self.assertNotEqual(engine.vector, [0.0] * 5)
         style = engine.get_personality_style()
