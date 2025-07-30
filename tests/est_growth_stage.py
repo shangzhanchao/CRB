ce2a1f04433474df38b2c28fb1888de1fad6e615
shangzhanchao <15863174575@163.com>
2025-07-30 08:48:02 +0800

    Update est_growth_stage.py

--- tests/est_growth_stage.py
+++ tests/est_growth_stage.py
@@ -4,8 +4,8 @@ from ai_core import global_state
 class TestGrowthStage(unittest.TestCase):
     def test_stage_progression(self):
         global_state.reset()
-        # Initial stage should be sprout
-        self.assertEqual(global_state.get_growth_stage(), "sprout")
+        # Initial stage should be enlighten by default
+        self.assertEqual(global_state.get_growth_stage(), "enlighten")
         # Simulate some interactions and audio
         global_state.INTERACTION_COUNT = 25
         global_state.AUDIO_DATA_SECONDS = 400
